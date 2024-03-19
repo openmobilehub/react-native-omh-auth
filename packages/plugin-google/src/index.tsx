835978a34c9971838ab104c69dd6f273a58d2153
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-19 16:52:34 +0100

    chore: init plugin-google

---
+++ packages/plugin-google/src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from "react-native";
+
+const LINKING_ERROR =
+  `The package 'rn-omh-auth-plugin-google' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: "" }) +
+  "- You rebuilt the app after installing the package\n" +
+  "- You are not using Expo Go\n";
+
+const RnOmhAuthPluginGoogle = NativeModules.RnOmhAuthPluginGoogle
+  ? NativeModules.RnOmhAuthPluginGoogle
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return RnOmhAuthPluginGoogle.multiply(a, b);
+}
