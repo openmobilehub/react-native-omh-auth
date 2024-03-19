f00a6a512018f009397f72d95869d6b27c4d2d75
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-19 16:52:46 +0100

    chore: init sample app

---
+++ apps/SampleApp/App.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import {} from 'react-native';
+
+import { StyleSheet, View, Text } from 'react-native';
+import { multiply } from '@omh-auth/plugin-google';
+
+export default function App() {
+  const [result, setResult] = React.useState<number | undefined>();
+
+  React.useEffect(() => {
+    multiply(3, 2).then(setResult);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>Result: {result}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
---
+++ apps/SampleApp/metro.config.js
@@ -0,0 +1,19 @@
+const {getDefaultConfig} = require('@react-native/metro-config');
+const path = require('path');
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+const config = getDefaultConfig(projectRoot);
+
+// Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+
+// Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
+module.exports = config;
