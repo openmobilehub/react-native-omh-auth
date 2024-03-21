fa998c29ce194d8cdfe84236cb96c9722e8a0c79
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-03-21 18:00:18 +0100

    chore: setup metro & move files around

--- apps/SampleApp/App.tsx
+++ apps/SampleApp/App.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import {} from 'react-native';
 
 import {StyleSheet, View, Text} from 'react-native';
-import {multiply} from '@omh-auth/plugin-google';
+import {multiply} from '@omh/react-native-auth-google';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
--- apps/SampleApp/metro.config.js
+++ apps/SampleApp/metro.config.js
@@ -10,4 +10,13 @@ const config = getDefaultConfig(projectRoot);
 // Watch all files within the monorepo
 config.watchFolders = [monorepoRoot];
 
+// Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
+// Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+config.resolver.disableHierarchicalLookup = true;
+
 module.exports = config;
