bdc41e18bab87e8185589f1b54c37331cbe936d1
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-04-04 13:20:55 +0200

    fix: setup tests correctly
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- apps/sample-app/setup-jest.ts
+++ apps/sample-app/setup-jest.ts
@@ -1,3 +1,5 @@
+import {jest} from '@jest/globals';
+
 jest.mock('react-native-safe-area-context', () =>
   require('react-native-safe-area-context/jest/mock'),
 );
@@ -6,4 +8,6 @@ jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
 
+// OMH
 jest.mock('@omh/react-native-auth-google', () => ({}));
+jest.mock('@omh/react-native-auth-facebook', () => ({}));
