453996243c545c7c66296294de3a1dac173d271c
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-04-09 11:01:29 +0200

    chore: setup docusaurus
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

---
+++ docs/docs/api/modules/core_src.md
@@ -0,0 +1,46 @@
+---
+id: "core_src"
+title: "Module: core/src"
+sidebar_label: "core/src"
+sidebar_position: 0
+custom_edit_url: null
+---
+
+## Classes
+
+- [AuthModule](../classes/core_src.AuthModule.md)
+
+## Type Aliases
+
+### BaseAuthConfig
+
+Ƭ **BaseAuthConfig**: `Object`
+
+#### Type declaration
+
+| Name | Type |
+| :------ | :------ |
+| `scopes` | `string`[] |
+
+#### Defined in
+
+[core/src/types.ts:1](https://github.com/openmobilehub/rn-omh-auth/blob/b43af64eae277675f57051591f993f84fd70cd48/packages/core/src/types.ts#L1)
+
+___
+
+### OmhUserProfile
+
+Ƭ **OmhUserProfile**: `Object`
+
+#### Type declaration
+
+| Name | Type |
+| :------ | :------ |
+| `email?` | `string` |
+| `name?` | `string` |
+| `profileImage?` | `string` |
+| `surname?` | `string` |
+
+#### Defined in
+
+[core/src/types.ts:5](https://github.com/openmobilehub/rn-omh-auth/blob/b43af64eae277675f57051591f993f84fd70cd48/packages/core/src/types.ts#L5)
---
+++ docs/src/pages/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Heading from '@theme/Heading';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+
+import HomepageFeatures from '../components/HomepageFeatures';
+import styles from './index.module.css';
+
+type HomepageLink = Record<'text' | 'link', string>;
+
+const HOMEPAGE_LINKS: HomepageLink[] = [
+  {
+    text: 'Get started 🚀',
+    link: '/docs/getting-started',
+  },
+  {
+    text: 'API reference 📚',
+    link: '/docs/api',
+  },
+  {
+    text: 'Contribute 🤝',
+    link: '/docs/contributing',
+  },
+];
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          {[
+            HOMEPAGE_LINKS.map(({text, link}, index) => (
+              <Link
+                key={index}
+                className={clsx(
+                  'button button--secondary button--lg',
+                  styles.homepageButton,
+                )}
+                to={link}>
+                {text}
+              </Link>
+            )),
+          ]}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
