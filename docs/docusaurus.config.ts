a1e89c245a4d1cd411db98895a59a3e74696da32
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-04-09 11:02:05 +0200

    ci: setup cd
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://special-barnacle-93vn82m.pages.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
