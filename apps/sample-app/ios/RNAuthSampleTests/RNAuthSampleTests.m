7983cf6b5a45f399d2dbed6e08c99811aa014fd6
Andrei Zgîrvaci <zgirvaciandrei@gmail.com>
2024-04-19 13:00:50 +0300

    Setup ios (#21)
    * chore: rename ios app
    
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>
    
    refactor: rename sample-app name
    
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>
    
    * ci: deploy .ipa to appcenter
    
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>
    
    ---------
    
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- apps/sample-app/ios/RNAuthSampleTests/RNAuthSampleTests.m
+++ apps/sample-app/ios/RNAuthSampleTests/RNAuthSampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SampleAppTests : XCTestCase
+@interface RNAuthSampleTests : XCTestCase
 
 @end
 
-@implementation SampleAppTests
+@implementation RNAuthSampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
