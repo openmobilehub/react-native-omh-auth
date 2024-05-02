372926f110ca26517fbe03cf90f8bca7ca7ebb8f
andrei-zgirvaci <zgirvaciandrei@gmail.com>
2024-05-02 13:56:30 +0200

    chore: add JSDocs for iOS module
    Signed-off-by: andrei-zgirvaci <zgirvaciandrei@gmail.com>

--- packages/core/src/IOSAuthModule.ts
+++ packages/core/src/IOSAuthModule.ts
@@ -22,11 +22,18 @@ export default class AuthModuleIOS implements IAuthModule {
 
   constructor(private moduleConfig: AuthModuleConfig) {}
 
+  /**
+   * Initializes the authentication provider with the given configuration.
+   * @param config Configuration object required for initialization.
+   */
   async initialize(config: BaseAuthConfig): Promise<void> {
     this.config = config;
     this.authData = await getPersistedAuthData();
   }
 
+  /**
+   * Initiates the sign-in process with the authentication provider.
+   */
   async signIn(): Promise<void> {
     const config = this.getConfig();
 
@@ -40,16 +47,28 @@ export default class AuthModuleIOS implements IAuthModule {
     }
   }
 
+  /**
+   * Retrieves the access token from the authentication provider.
+   * @returns The access token if available, otherwise undefined.
+   */
   async getAccessToken(): Promise<string | undefined> {
     const authData = this.getAuthData();
 
     return authData.accessToken;
   }
 
+  /**
+   * Obtains user information for the current signed-in user.
+   * @returns An object containing user profile information.
+   */
   async getUser(): Promise<OmhUserProfile | undefined> {
     return this.moduleConfig.IOSGetUser(this.getAuthData);
   }
 
+  /**
+   * Refreshes the access token if it has expired.
+   * @returns The new access token if refreshed, otherwise undefined.
+   */
   async refreshAccessToken(): Promise<string | undefined> {
     if (this.moduleConfig.IOSRefreshAccessToken instanceof Function) {
       return this.moduleConfig.IOSRefreshAccessToken(this.getAuthData);
@@ -73,6 +92,9 @@ export default class AuthModuleIOS implements IAuthModule {
     return accessToken;
   }
 
+  /**
+   * Revokes the current access token.
+   */
   async revokeAccessToken(): Promise<void> {
     if (this.moduleConfig.IOSRevokeAccessToken instanceof Function) {
       return this.moduleConfig.IOSRevokeAccessToken(this.getAuthData);
@@ -84,6 +106,9 @@ export default class AuthModuleIOS implements IAuthModule {
     await revoke(config, {tokenToRevoke: authData.accessToken});
   }
 
+  /**
+   * Initiates the sign-out process with the authentication provider.
+   */
   async signOut(): Promise<void> {
     await removePersistedAuthData();
 
