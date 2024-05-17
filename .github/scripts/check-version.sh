9644f8268eef35cfb01b30335b3909a24de7b17c
Kacper Wiszczuk <kacperwiszczuk@gmail.com>
2024-05-17 11:08:54 +0200

    fix: release script
    Signed-off-by: Kacper Wiszczuk <kacperwiszczuk@gmail.com>

--- .github/scripts/check-version.sh
+++ .github/scripts/check-version.sh
@@ -1,10 +1,11 @@
-echo "PACKAGE_VERSION=$(jq -r .version < lerna.json)" >> $GITHUB_ENV
+PACKAGE_VERSION=$(jq -r .version < lerna.json)
 git fetch --tags
 
-if git tag | grep -x "v${{ env.PACKAGE_VERSION }}" > /dev/null; then
-  echo "Version ${{ env.PACKAGE_VERSION }} already exists, no new tag created."
+if git tag | grep -x "v${PACKAGE_VERSION}" > /dev/null; then
+  echo "Version ${PACKAGE_VERSION} already exists, no new tag created."
   echo "version_changed=false" >> $GITHUB_ENV
 else
-  echo "New version detected: ${{ env.PACKAGE_VERSION }}"
+  echo "New version detected: ${PACKAGE_VERSION}"
   echo "version_changed=true" >> $GITHUB_ENV
+  echo "PACKAGE_VERSION=${PACKAGE_VERSION}" >> $GITHUB_ENV
 fi
