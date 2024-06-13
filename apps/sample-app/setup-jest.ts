1148182783553f6cf4e9a428ae287778c61cf3e0
Damian Nowak <damian.nowak.5f@gmail.com>
2024-06-13 10:08:00 +0200

    docs: add features animations to README (#78)
    Signed-off-by: DamianNowak5f <damian.nowak.5f@gmail.com>

--- apps/sample-app/setup-jest.ts
+++ apps/sample-app/setup-jest.ts
@@ -7,7 +7,3 @@ jest.mock('react-native-safe-area-context', () =>
 jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
-
-// OMH
-jest.mock('@openmobilehub/auth-google', () => ({}));
-jest.mock('@openmobilehub/auth-facebook', () => ({}));
