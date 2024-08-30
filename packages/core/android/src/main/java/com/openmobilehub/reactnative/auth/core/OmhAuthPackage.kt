903549c19d8e8d4e81a0148c9471694d9b894628
Maciej Budziński <maciej.budzinski.93@gmail.com>
2024-08-30 11:47:39 +0200

    feat/OMHD-542: Autolinking improvements (#87)
    * feat: enable autolinking
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * feat: update sample app
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * docs: update docs
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    * fix: changes after CR
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>
    
    ---------
    
    Signed-off-by: Maciej Budziński <maciej.budzinski.93@gmail.com>

---
+++ packages/core/android/src/main/java/com/openmobilehub/reactnative/auth/core/OmhAuthPackage.kt
@@ -0,0 +1,17 @@
+package com.openmobilehub.reactnative.auth.core
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+// Required for autolinking
+class OmhAuthCorePackage : ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+        return emptyList()
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+        return emptyList()
+    }
+}
